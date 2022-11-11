f6ea8636ddc3b43050c20175dc056d1daa205aee
Vlad Danilov <vladdanilov999@gmail.com>
2022-11-11 18:27:03 +0200

    Maybe Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const gallaryMurkUp = createGallaryMarkUp(galleryItems);
+
+galleryRef.insertAdjacentHTML('afterbegin', gallaryMurkUp);
+
+function createGallaryMarkUp(images) {
+  return images
+    .map(({ original, preview }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image description"
+            />
+        </a>
+    </div>
+      `;
+    })
+    .join('');
+}
+
+galleryRef.addEventListener('click', onGallary);
+
+function onGallary(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.getAttribute('src')}" width="800" height="600">
+`);
+
+  instance.show();
+
+  if (instance.show()) {
+    window.addEventListener('keydown', onKeyClose);
+
+    function onKeyClose(evt) {
+      if (evt.key === 'Escape') {
+        instance.close();
+      }
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryListRef = document.querySelector('.gallery');
+
+gallaryListRef.insertAdjacentHTML('afterbegin', createGallaryMarkUp(galleryItems));
+
+function createGallaryMarkUp(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+gallaryListRef.addEventListener('click', onClickGallary);
+
+function onClickGallary(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  `
+    <div class="gallery">
+      <a href="${e.target.getAttribute('href')}">
+        <img src="${e.target.getAttribute('src')}" alt="" title="${e.target.getAttribute('let')}" />
+        </a>
+         <a href="${e.target.getAttribute('href')}">
+        <img src="${e.target.getAttribute('src')}" alt="" title="${e.target.getAttribute('let')}" />
+        </a>
+    </div>;
+   `;
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function () {
+    // Do something…
+  });
+}
